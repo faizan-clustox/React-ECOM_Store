72e8bac0326efb7de881aedf017879a9da6d68b8
Faizan Ahmad <faizan.ahmad@clustox.com>
2024-01-08 15:24:17 +0500

    Initial Commmit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StoreGrid from "../storeGrid/StoreGrid";
+
+const Home = () => {
+  return (
+    <div>
+      <StoreGrid />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/storeGrid/StoreGrid.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import StoreItem from "../storeItem/StoreItem";
+
+const StoreGrid = () => {
+  return <StoreItem />;
+};
+
+export default StoreGrid;
