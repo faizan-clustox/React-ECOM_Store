1491808036d6a1aec90dc5284f9fbad3d8aa8f5b
Faizan Ahmad <faizan.ahmad@clustox.com>
2024-01-08 15:49:23 +0500

    Added grid and responsiveness.

--- src/components/storeGrid/StoreGrid.jsx
+++ src/components/storeGrid/StoreGrid.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import StoreItem from "../storeItem/StoreItem";
 
 const StoreGrid = () => {
-  return <StoreItem />;
+  return (
+    <>
+      <div class="mx-auto p-4 grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 bg-white dark:bg-gray-800">
+        <StoreItem />
+        <StoreItem />
+        <StoreItem />
+        <StoreItem />
+      </div>
+    </>
+  );
 };
 
 export default StoreGrid;
